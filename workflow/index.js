9544595f837eeeca99c2fdcf8227d959db9a0170
Bence Balazs <56939786+benceBalazs@users.noreply.github.com>
2022-04-21 15:09:22 +0200

    created travel_extended

--- workflow/index.js
+++ workflow/index.js
@@ -1,12 +1,121 @@
-var fs = require("fs");
-const csv = require("csvtojson");
+import fs from "fs";
+import csv from "csvtojson";
+import fetch from 'node-fetch';
 
 let directory = "../3_Car_sharing/";
 
 console.log("-- Welcome to the Articonf-2022 Dataset Analyzer --\n");
 console.log("1. Converting .csv to .json");
-createFolder("findings");
-convertToJSON();
+
+// createFolder("findings");
+// convertToJSON();
+
+console.log("2. Adjust .json to include states calculated by lat and long");
+
+// addFormatUsers();
+// addLocation();
+
+console.log("3. Adjust .json regarding csv compatibility");
+
+adjustSuggestions();
+
+function adjustSuggestions() {
+  let tempTravelFile = JSON.parse(fs.readFileSync(directory+"findings/Travel_ext.json").toString());
+  suggestions(tempTravelFile);
+}
+
+function addLocation() {
+  let tempTravelFile = JSON.parse(fs.readFileSync(directory+"findings/Travel_ext.json").toString());
+  asyncLoopEndLocations(tempTravelFile);
+}
+
+function suggestions(tempTravelFile){
+  tempTravelFile.forEach((travelInstance)=>{
+    let saveSuggestionStart = travelInstance.startedBy;
+    let saveSuggestionEnd = travelInstance.suggestedEndPlaces;
+    travelInstance.startedBy = {};
+    travelInstance.suggestedEndPlaces = {};
+    JSON.parse(saveSuggestionStart).forEach((start, index)=>{
+      travelInstance.startedBy["coordinate"+index] = {"latitude": start.coordinate.latitude, "longitude": start.coordinate.longitude};
+      travelInstance.startedBy["moment"] = start.moment;
+    });
+    JSON.parse(saveSuggestionEnd).forEach((end)=>{
+      travelInstance.suggestedEndPlaces[end.suggestedBy] = {"latitude": end.endPlace.latitude, "longitude": end.endPlace.longitude};
+    });
+  });
+  fs.writeFile(
+    directory + "/findings/" + "Travel_ext.json",
+    JSON.stringify(tempTravelFile, undefined, 2),
+    function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    }
+  );
+}
+
+async function asyncLoopStartLocations(travelInstance){
+  let counter = travelInstance.length;
+  for (let index = 0; index < travelInstance.length; index++) {
+    const response = await fetch('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat='+travelInstance[index].startPlace.latitude+'&lon='+travelInstance[index].startPlace.longitude);
+    let requestedLocation = await response.json();
+    console.log("Requested information: "+ counter-- +" instances left...");
+    Object.assign(travelInstance[index].startPlace, {"country": requestedLocation.address.country});
+    // travelInstance.startPlace.country = requestedLocation.address.country;
+    await sleep(3000); // in accordance to the api
+  }
+  fs.writeFile(
+    directory + "/findings/" + "Travel_ext.json",
+    JSON.stringify(travelInstance, undefined, 2),
+    function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    }
+  );
+}
+
+async function asyncLoopEndLocations(travelInstance){
+  let counter = travelInstance.length;
+  for (let index = 0; index < travelInstance.length; index++) {
+    const response = await fetch('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat='+travelInstance[index].endPlace.latitude+'&lon='+travelInstance[index].endPlace.longitude);
+    let requestedLocation = await response.json();
+    console.log("Requested information: "+ counter-- +" instances left...");
+    Object.assign(travelInstance[index].endPlace, {"country": requestedLocation.address.country});
+    // travelInstance.startPlace.country = requestedLocation.address.country;
+    await sleep(3000); // in accordance to the api
+  }
+  fs.writeFile(
+    directory + "/findings/" + "Travel_ext.json",
+    JSON.stringify(travelInstance, undefined, 2),
+    function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    }
+  );
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function addFormatUsers() {
+  let tempTravelFile = JSON.parse(fs.readFileSync(directory+"findings/Travel.json").toString());
+  tempTravelFile.forEach((travelInstance)=>{
+    travelInstance["users_formatted"] = {};
+    JSON.parse(travelInstance.users).forEach((user)=>{
+      travelInstance["users_formatted"][user.userId] = {"passengers": user.passengers};
+    });
+  });
+  fs.writeFile(
+    directory + "/findings/" + "Travel_ext.json",
+    JSON.stringify(tempTravelFile, undefined, 2),
+    function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    }
+  );
+}
 
 function convertToJSON() {
   fs.readdirSync(directory).forEach(file => {
